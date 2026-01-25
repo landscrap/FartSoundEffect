decbe006635b0edd1d28d98b0e9ee2b8a00a0a16
popcorn <scandium935@gmail.com>
2026-01-25 21:37:45 +0100

    0.0.1 (released mod, thanks kam!)

---
+++ about.md
@@ -0,0 +1,7 @@
+you know what this mod does?
+
+almost nothing, that's right! but you know what it __does__ do?
+
+it adds a button to your main menu that produces a fart sound effect!!!!!!! how amazing!!!
+
+developed by popcorn, huge thanks to kam (km7dev) for help
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <random>
+
+using namespace geode::prelude;
+
+class $modify(FartMenuLayer, MenuLayer) {
+
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        auto sprite = CCSprite::create("buttonthing.png"_spr);
+        sprite->setScale(0.5f);
+
+        auto button = CCMenuItemSpriteExtra::create(
+            sprite,
+            this,
+            menu_selector(FartMenuLayer::onFart)
+        );
+        button->setID("fart-button"_spr);
+
+        if (auto menu = this->getChildByID("bottom-menu")) {
+            menu->addChild(button);
+            menu->updateLayout();
+        }
+
+        return true;
+    }
+
+    void onFart(CCObject*) {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dist(0.6f, 1.4f);
+        FMODAudioEngine::get()->playEffectAdvanced("fart.mp3"_spr, 1, 0, 1, dist(gen), false, false, 0, 0, 0, 0, false, 0, false, false, 0, 0, 0, 0);
+    }
+};
